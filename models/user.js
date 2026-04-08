af6c9ddb5a54d190b711b76520231cef3f288ce8
Gabriel Martins <gab.smartinn@outlook.com>
2026-04-08 16:42:07 -0300

    feat: create `user.findOneByUsername` method and `/api/v1/users/[username]` endpoint

--- models/user.js
+++ models/user.js
@@ -1,5 +1,36 @@
 import database from "infra/database";
-import { ValidationError } from "infra/errors";
+import { NotFoundError, ValidationError } from "infra/errors";
+
+async function findOneByUsername(username) {
+  const userFound = await runSelectQuery(username);
+
+  return userFound;
+
+  async function runSelectQuery(username) {
+    const results = await database.query({
+      text: `
+        SELECT 
+          * 
+        FROM
+          users
+        WHERE
+          LOWER(username) = LOWER($1)
+        LIMIT
+          1
+      ;`,
+      values: [username]
+    });
+
+    if (results.rowCount < 1) {
+      throw new NotFoundError({
+        message: "O username informado não foi encontrado nos sistema.",
+        action: "Verifique se o username está digitado corretamente.",
+      })
+    }
+
+    return results.rows[0]
+  }
+}
 
 async function create(userInputValues) {
   await validateUniqueEmail(userInputValues.email)
@@ -73,7 +104,8 @@ async function create(userInputValues) {
 }
 
 const user = {
-  create
+  create,
+  findOneByUsername
 }
 
 export default user;
