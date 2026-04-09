8a54e052596e5c7407c15b3fa8ad825ead1d551a
Gabriel Martins <gab.smartinn@outlook.com>
2026-04-09 17:54:24 -0300

    feat: create and use `orchestrator.createUser()`

--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -1,6 +1,10 @@
-import database from "infra/database";
 import retry from "async-retry";
+import { faker } from "@faker-js/faker"
+
+
+import database from "infra/database.js";
 import migrator from "models/migrator.js";
+import user from "models/user.js";
 
 const waitForAllServices = async () => {
   await waitForWebServer();
@@ -27,10 +31,19 @@ const runPendingMigrations = async () => {
   await migrator.runPendingMigrations();
 };
 
+const createUser = async (userObject) => {
+  return await user.create({
+    username: userObject.username || faker.internet.username().replace(/[_.-]/g, ""),
+    email: userObject.email || faker.internet.email(),
+    password: userObject.password || "valid_password"
+  })
+}
+
 const orchestrator = {
   waitForAllServices,
   clearDatabase,
   runPendingMigrations,
+  createUser
 };
 
 export default orchestrator;
